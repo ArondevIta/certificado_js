a2e9ed26c3a367639baccacf9172e5834beb7b9e
Aron Madson de Tarso Sousa Amaral <ba20395388@internal.cambuci.com.br>
2020-11-05 17:21:46 -0300

    pagina de login

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
 import { Container, Button, Row, Col, Image } from "react-bootstrap";
+
+import Menu from "../Menu";
+import Footer from "../Footer";
 
 import certificado1 from "../../assets/certificado1.jpg";
 import certificado2 from "../../assets/certificado2.jpg";
@@ -8,51 +13,65 @@ import certificado3 from "../../assets/certificado3.png";
 import "./style.css";
 
 function Home() {
+  const history = useHistory();
+  function goToLogin() {
+    history.push("/login");
+  }
+
   return (
-    <Container id="container">
-      <h1>Certificado Legal</h1>
-      <p>
-        Gere seu certificado de onde quiser, apenas tendo acesso a internet!
-      </p>
+    <>
+      <Menu />
+      <Container id="container">
+        <h1>Certificado Legal</h1>
+        <p>
+          Gere seu certificado de onde quiser, apenas tendo acesso a internet!
+        </p>
 
-      <Button className="butao" variant="primary" size="lg">
-        Login
-      </Button>
-      <Button
-        style={{ marginLeft: 15, width: 200 }}
-        variant="outline-primary"
-        size="lg"
-      >
-        Cadastre-se
-      </Button>
+        <Button
+          onClick={goToLogin}
+          className="butao"
+          variant="primary"
+          size="lg"
+        >
+          Login
+        </Button>
+        <Button
+          style={{ marginLeft: 15, width: 200 }}
+          variant="outline-primary"
+          size="lg"
+        >
+          Cadastre-se
+        </Button>
 
-      <Row className="row-img">
-        <Col sm={4}>
-          <h3>Gere seus certificado</h3>
-          <Image className="img-fluid" src={certificado1} rounded />
-          <p>
-            Com o certificado legal você pode gerar certificados para seus
-            alunos de onde estiver.
-          </p>
-        </Col>
-        <Col sm={4}>
-          <h3>Verifique se é válido</h3>
-          <Image className="img-fluid" src={certificado2} rounded />
-          <p>
-            Você pode verificar se ele é válido, através do nosso sistema de
-            pesquisa
-          </p>
-        </Col>
-        <Col sm={4}>
-          <h3>Imprima de onde estiver</h3>
-          <Image className="img-fluid" src={certificado3} rounded />
-          <p>
-            E você pode imprimir ou baixar em qualquer lugar, basta apenas ter
-            acesso a internet!
-          </p>
-        </Col>
-      </Row>
-    </Container>
+        <Row className="row-img">
+          <Col sm={4}>
+            <h3>Gere seus certificado</h3>
+            <Image className="img-fluid" src={certificado1} rounded />
+            <p>
+              Com o certificado legal você pode gerar certificados para seus
+              alunos de onde estiver.
+            </p>
+          </Col>
+          <Col sm={4}>
+            <h3>Verifique se é válido</h3>
+            <Image className="img-fluid" src={certificado2} rounded />
+            <p>
+              Você pode verificar se ele é válido, através do nosso sistema de
+              pesquisa
+            </p>
+          </Col>
+          <Col sm={4}>
+            <h3>Imprima de onde estiver</h3>
+            <Image className="img-fluid" src={certificado3} rounded />
+            <p>
+              E você pode imprimir ou baixar em qualquer lugar, basta apenas ter
+              acesso a internet!
+            </p>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </>
   );
 }
 
--- web/src/pages/Login/index.js
+++ web/src/pages/Login/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 
-// import { Container } from './styles';
+import { Container, Form, Button } from "react-bootstrap";
+
+import "./style.css";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <Container id="content">
+      <h4>Certificado Legal</h4>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control type="email" placeholder="Digite seu Email" required />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Control type="password" placeholder="Insira sua senha" />
+        </Form.Group>
+        <Button className="btn-login" type="submit" variant="dark">
+          Entrar
+        </Button>
+      </Form>
+    </Container>
+  );
 }
 
 export default Login;
