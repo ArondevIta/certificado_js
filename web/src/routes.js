09b39203418f1e670ecd01566c7ff836dcb86b98
Aron Madson de Tarso Sousa Amaral <ba20395388@internal.cambuci.com.br>
2020-11-06 09:59:28 -0300

    tela de cadastro

--- web/src/routes.js
+++ web/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function Routes() {
   return (
@@ -11,6 +12,7 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
   );
