779e0e94fcc00948e7f69d206a81742b99b32189
ArondevIta <aronamaral@gmail.com>
2020-11-14 13:17:15 -0300

    estudantes

--- web/src/pages/Dashboard/Students/index.js
+++ web/src/pages/Dashboard/Students/index.js
@@ -1,105 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Container, Card, Col, Row } from "react-bootstrap";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import { useHistory } from "react-router-dom";
 
 import Menu from "../Menu";
+import api from "../../../services/api";
 
 import "./style.css";
 
 function Students() {
+  const [students, setStudents] = useState([]);
+  const history = useHistory();
+
+  const userId = localStorage.getItem("userId");
+  const token = localStorage.getItem("token");
+
+  // async function loadStudents() {
+  //   const response = await api.get("students", {
+  //     headers: { Authorization: `Bearer ${token}` },
+  //   });
+
+  //   setStudents(response.data);
+
+  //   console.log(students);
+  // }
+
+  useEffect(() => {
+    async function loadSpots() {
+      const userId = localStorage.getItem("userId");
+      const token = localStorage.getItem("token");
+      const response = await api.get("students", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      setStudents(response.data);
+      console.log(response.data);
+    }
+    loadSpots();
+  }, []);
+
   return (
     <>
       <Menu />
       <Container style={{ marginTop: 50 }}>
         <Row>
-          <Col sm={12} md={6} lg={4}>
-            <Card className="card-students">
-              <Card.Header
-                style={{ color: "#ffffff", background: "#3d3d3d" }}
-                as="h6"
-              >
-                Aron Madson de Tarso Sousa do Amaral
-              </Card.Header>
-              <Card.Body style={{ margin: 0 }}>
-                <p>
-                  <strong className="text-card">Matricula:</strong> 2165465
-                  <FaEdit
-                    className="icon-card"
-                    style={{ color: "#DAA520", float: "right" }}
-                  />
-                </p>
-                <p>
-                  <strong className="text-card">Estado:</strong> BA
-                </p>
-                <p>
-                  <strong className="text-card">Cidade:</strong> Itabuna
-                  <FaTrashAlt
-                    className="icon-card"
-                    style={{ float: "right", color: "crimson" }}
-                  />
-                </p>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col sm={12} md={6} lg={4}>
-            <Card className="card-students">
-              <Card.Header
-                style={{ color: "#ffffff", background: "#3d3d3d" }}
-                as="h6"
-              >
-                Aron Madson de Tarso Sousa do Amaral
-              </Card.Header>
-              <Card.Body style={{ margin: 0 }}>
-                <p>
-                  <strong className="text-card">Matricula:</strong> 2165465
-                  <FaEdit
-                    className="icon-card"
-                    style={{ color: "#DAA520", float: "right" }}
-                  />
-                </p>
-                <p>
-                  <strong className="text-card">Estado:</strong> BA
-                </p>
-                <p>
-                  <strong className="text-card">Cidade:</strong> Itabuna
-                  <FaTrashAlt
-                    className="icon-card"
-                    style={{ float: "right", color: "crimson" }}
-                  />
-                </p>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col sm={12} md={6} lg={4}>
-            <Card className="card-students">
-              <Card.Header
-                style={{ color: "#ffffff", background: "#3d3d3d" }}
-                as="h6"
-              >
-                Aron Madson de Tarso Sousa do Amaral
-              </Card.Header>
-              <Card.Body style={{ margin: 0 }}>
-                <p>
-                  <strong className="text-card">Matricula:</strong> 2165465
-                  <FaEdit
-                    className="icon-card"
-                    style={{ color: "#DAA520", float: "right" }}
-                  />
-                </p>
-                <p>
-                  <strong className="text-card">Estado:</strong> BA
-                </p>
-                <p>
-                  <strong className="text-card">Cidade:</strong> Itabuna
-                  <FaTrashAlt
-                    className="icon-card"
-                    style={{ float: "right", color: "crimson" }}
-                  />
-                </p>
-              </Card.Body>
-            </Card>
-          </Col>
+          {/* {students.map((student) => (
+            <Col key={student.id} sm={12} md={6} lg={4}>
+              <Card className="card-students">
+                <Card.Header
+                  style={{ color: "#ffffff", background: "#3d3d3d" }}
+                  as="h6"
+                >
+                  {student.name}
+                </Card.Header>
+                <Card.Body style={{ margin: 0 }}>
+                  <p>
+                    <strong className="text-card">Matricula:</strong>
+                    {student.registration}
+                    <FaEdit
+                      className="icon-card"
+                      style={{ color: "#DAA520", float: "right" }}
+                    />
+                  </p>
+                  <p>
+                    <strong className="text-card">Estado:</strong> {student.uf}
+                  </p>
+                  <p>
+                    <strong className="text-card">Cidade:</strong>
+                    {student.city}
+                    <FaTrashAlt
+                      className="icon-card"
+                      style={{ float: "right", color: "crimson" }}
+                    />
+                  </p>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))} */}
         </Row>
       </Container>
     </>
