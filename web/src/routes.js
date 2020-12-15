b05fffca84ee0d9844c592080d684687549f14ec
ArondevIta <aronamaral@gmail.com>
2020-12-15 20:08:36 -0300

    rotas privadas

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+
+import { isAuthenticated } from "./services/auth";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -12,6 +14,21 @@ import Certificates from "./pages/Dashboard/Certificates";
 import EditCertificate from "./pages/Dashboard/Certificates/EditCertificate";
 import StudentCertificate from "./pages/DashboardStudent/Certificates";
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{ pathname: "/login", state: { from: props.location } }}
+        />
+      )
+    }
+  />
+);
+
 function Routes() {
   return (
     <BrowserRouter>
@@ -19,16 +36,20 @@ function Routes() {
         <Route path="/" exact component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        <Route path="/dashboard" exact component={Dashboard} />
-        <Route path="/dashboard/certificates" exact component={Certificates} />
-        <Route
+        <PrivateRoute path="/dashboard" exact component={Dashboard} />
+        <PrivateRoute
+          path="/dashboard/certificates"
+          exact
+          component={Certificates}
+        />
+        <PrivateRoute
           path="/dashboard/certificates/edit"
           exact
           component={EditCertificate}
         />
-        <Route path="/dashboard/students" exact component={Students} />
-        <Route path="/student" exact component={DashboardStudent} />
-        <Route
+        <PrivateRoute path="/dashboard/students" exact component={Students} />
+        <PrivateRoute path="/student" exact component={DashboardStudent} />
+        <PrivateRoute
           path="/student/certificates"
           exact
           component={StudentCertificate}
