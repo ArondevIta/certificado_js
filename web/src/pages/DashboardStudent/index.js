395be94ff9c5a6fd5783fa623dfba5de81c134da
Aron Madson de Tarso Sousa Amaral <ba20395388@internal.cambuci.com.br>
2021-01-05 16:45:04 -0300

    nome pagina inicial

--- web/src/pages/DashboardStudent/index.js
+++ web/src/pages/DashboardStudent/index.js
@@ -3,9 +3,12 @@ import React from "react";
 import Menu from "./Menu";
 
 function DashboardStudent() {
+  const token = localStorage.getItem("token");
+  const id = localStorage.getItem("userId");
   return (
     <>
       <Menu />
+      <h1 style={{ color: "#FFF" }}>{id}</h1>
     </>
   );
 }
